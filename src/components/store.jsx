664397f4b1df89d87fe4e4cea64daf2788efe1e8
CarlosCardenas24 <nooptions101@gmail.com>
2023-03-30 19:57:39 -0700

    finished productStore

--- src/components/store.jsx
+++ src/components/store.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import  productStore  from '../productStore'
+import  {produce, getProductData}  from '../productStore'
 import { Row, Col } from 'react-bootstrap'
-import { render } from '@testing-library/react'
 
 function Store() {
-  
+
   return (
     <div>
       <Row>
-        
+        {produce.map((product, index) => <Col key={index}>{product.title} <br /> {product.price}</Col>)}
       </Row>
     </div>
   )
